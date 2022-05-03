5404baac4b051358b20c7ce48e585a15cde85d37
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2022-05-03 20:53:57 -0300

    Usando FaunaDB - Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb'
+
 import NextAuth from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github';
+import GitHubProvider from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({ 
 
@@ -8,5 +12,26 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     })
-  ]
+  ],
+  jwt: {
+    secret: process.env.SIGNING_KEY 
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const userEmail  = user.email
+
+      try { 
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            { data: { email: userEmail } }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
