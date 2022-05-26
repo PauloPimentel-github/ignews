784a753e65427175f1691d771dfd67f0f6711bbf
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2022-05-25 22:17:56 -0300

    Trabalhando com CMS - Compoenente: ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link';
-
 import { SigInButton } from '../SigInButton';
 
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
@@ -10,12 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" title="ig.news" alt="[ig.news]" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts">
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
         <SigInButton />
       </div>
